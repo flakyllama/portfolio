1795729479d545c0cdb0917e8948f1da778b9619
Fardeen Chowdhury <56769908+flakyllama@users.noreply.github.com>
2020-07-26 20:38:34 -0400

    Added Gatsby Image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,9 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+  `gatsby-plugin-sass`, 
+  `gatsby-transformer-sharp`, 
+  `gatsby-plugin-sharp`
+],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react"
+import Image from "../components/image"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const IndexPage = () => (
+  <p>Home</p>
+)
+
+export default IndexPage
