507c52adf0734fd826db0927800d1c0833c825bd
Fardeen Chowdhury <fhchowdh@edu.uwaterloo.ca>
2021-03-14 01:58:13 -0500

    Created animated elements template and first element on Novuum

--- src/components/btnTop.js
+++ src/components/btnTop.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/global.scss'
 import ArrowUp from '../images/icons/arrow-up.svg'
 
